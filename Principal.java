12fa73f8155e272582387d5c3d99501f7e0482b4
Tonalli Isaias Garcia Garcia <tonalligarcia1994@gmail.com>
2024-04-06 23:18:02 -0600

    Inicialización de cliente desde clase principal

--- Principal.java
+++ Principal.java
@@ -1,5 +1,6 @@
 public class Principal{
     public static void main(String [] args){
         System.out.println("Hola mundo");
+        Cliente Cliente1 = new Cliente("Tonny Statk", "Cuenta corriente", 19000.99);
     }
 }
