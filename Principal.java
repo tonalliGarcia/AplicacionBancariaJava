8a39fc12369d3759033d799f7a3ba1ee027eb228
Tonalli Isaias Garcia Garcia <tonalligarcia1994@gmail.com>
2024-04-06 23:55:05 -0600

    Compilacion y ejecucion del archivo Principal.java

--- Principal.java
+++ Principal.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
+
 public class Principal{
     public static void main(String [] args){
-        System.out.println("Hola mundo");
-        Cliente Cliente1 = new Cliente("Tonny Statk", "Cuenta corriente", 19000.99);
+        Cliente cliente = new Cliente("Tony Stark", "Cuenta Corriente", 1599.99);
+        Banco banco = new Banco(cliente);
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("Menú:");
+            System.out.println("1. Consultar saldo");
+            System.out.println("2. Retirar");
+            System.out.println("3. Depositar");
+            System.out.println("9. Salir");
+            System.out.print("Elija una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    banco.consultarSaldo();
+                    break;
+                case 2:
+                    System.out.print("Ingrese la cantidad a retirar: ");
+                    double cantidadRetiro = scanner.nextDouble();
+                    banco.retirar(cantidadRetiro);
+                    break;
+                case 3:
+                    System.out.print("Ingrese la cantidad a depositar: ");
+                    double cantidadDeposito = scanner.nextDouble();
+                    banco.depositar(cantidadDeposito);
+                    break;
+                case 9:
+                    System.out.println("Finalizando el programa. ¡Gracias por usar nuestros servicios!");
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        } while (opcion != 9);
+
+        scanner.close();
     }
 }
